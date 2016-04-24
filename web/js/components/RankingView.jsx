d1b7e864f955dab4b40f1fad479c8c5026aab561
Hanzhou Shi <hanzhou87@gmail.com>
2016-04-24 11:11:57 -0700

    Now RankingView can correctly update.

--- web/js/components/RankingView.jsx
+++ web/js/components/RankingView.jsx
@@ -1,5 +1,6 @@
 var d3 = require('d3');
 var React = require('react');
+var Utils = require('../utils/Utils');
 
 var vis, w, h, yScale, yAxis, yAxisGroup, indexByDomain, tooltip;
 
@@ -18,17 +19,27 @@ var RankingView = React.createClass({
     .attr("transform", "translate(" + m.l + "," + m.t + ")");
 
     indexByDomain = {};
-    yScale = d3.scale.linear().range([h, 0]);
+    yScale = d3.scale.linear().range([h, 0]).domain([0, 10]);
 
     yAxis = d3.svg.axis()
     .scale(yScale)
     .orient("left")
-    .ticks(10, "s")
-    .outerTickSize(1);
+    .ticks(10, "s");
+
     yAxisGroup = vis.append("g")
     .attr("class", "axis")
-    .attr("transform", "translate(10,0)");
+    .attr("transform", "translate(10,0)").call(yAxis);
 
+    yAxisGroup.selectAll("line").data(yScale.ticks(100), function(d) { return d; })
+    .enter()
+    .append("line")
+    .attr("class", "minor")
+    .attr("x1", 0)
+    .attr("y1", yScale)
+    .attr("x2", -3)
+    .attr("y2", yScale);
+
+      yAxisGroup.call(yAxis);
     tooltip = d3.select("body").append("div" )
     .attr("id", "detail")
     .style("opacity", 0);
@@ -45,9 +56,6 @@ var RankingView = React.createClass({
     if (minmax && minmax.length == 2) {
       var minRating = minmax[0];
       var maxRating = minmax[1];
-      var yDomain = [minRating, maxRating];
-      yScale.domain(yDomain);
-      yAxisGroup.call(yAxis);
 
       // initialize indexByDomain
       this.insertIndexByRange(minmax, lo, hi);
@@ -94,8 +102,13 @@ var RankingView = React.createClass({
         if (parts.length == 2) {
           var lo = parts[0];
           var hi = parts[1];
-          var yMin = yScale(hi);
-          var yMax = yScale(lo);
+          var hiVal = Utils.pointOneCeil(hi);
+          var loVal = Utils.pointOneFloor(lo);
+          if (hiVal == loVal) {
+            loVal -= .1
+          }
+          var yMin = yScale(hiVal);
+          var yMax = yScale(loVal);
           rects.push({x: 0, y: yMin, w: w, h: yMax - yMin, lo: lo, hi: hi});
         }
       }
@@ -110,6 +123,30 @@ var RankingView = React.createClass({
       items += this.getAnimeListItem(ratings[i])
     }
     return "<ul>" + items + "</ul>"
+  },
+
+  findDivideIndex(ratings, lo, hi) {
+    var mid = Math.floor(lo + (hi - lo) / 2);
+    var midVal = ratings[mid]["rating"];
+    // find the smallest value that is greater than Utils.pointOneCeil(midVal)
+    // eventually mid should pointing to this value, since for a range
+    // displayed it doesn't include the lower bound.
+    var result = mid;
+    var loVal = Utils.pointOneFloor(midVal);
+    if (ratings[mid]["rating"] > loVal) {
+      var prev = result;
+      while (result <= hi && ratings[result]["rating"] > loVal) {
+        prev = result;
+        result += 1;
+      }
+      result = prev;
+    }
+    else {// could only be equal
+      while (result >= 0 && ratings[result]["rating"] == loVal) {
+        result -= 1;
+      }
+    }
+    return result
   },
 
   getAnimeListItem(ratingItem) {
@@ -129,16 +166,17 @@ var RankingView = React.createClass({
     .on("click", function(d) {
       var key = d.lo + "_" + d.hi;
       var indices = indexByDomain[key];
-      delete indexByDomain[key];
       // delete the original range, and insert two new ranges that
       // are halves from the original range
-      var ilo = indices[0], ihi = indices[1], mid = Math.floor((ilo + ihi) / 2);
-      var minmaxLo = component.minmaxWithinIndices(ratings, ilo, mid);
-      component.insertIndexByRange(minmaxLo, ilo, mid);
-      var minmaxHi = component.minmaxWithinIndices(ratings, mid, ihi);
-      component.insertIndexByRange(minmaxHi, mid + 1, ihi);
-
-      component.visualize();
+      var ilo = indices[0], ihi = indices[1], mid = component.findDivideIndex(ratings, ilo, ihi)
+      if (ilo < ihi && mid + 1 <= ihi) {
+        delete indexByDomain[key];
+        var minmaxLo = component.minmaxWithinIndices(ratings, ilo, mid);
+        component.insertIndexByRange(minmaxLo, ilo, mid);
+        var minmaxHi = component.minmaxWithinIndices(ratings, mid + 1, ihi);
+        component.insertIndexByRange(minmaxHi, mid + 1, ihi);
+        component.visualize();
+      }
     })
     .on("mouseover", function(d) {
       var key = d.lo + "_" + d.hi;
---
+++ web/js/utils/Utils.js
@@ -0,0 +1,11 @@
+module.exports = {
+
+  pointOneCeil(val) {
+    return Math.ceil(val * 10) / 10;
+  },
+
+  pointOneFloor(val) {
+    return Math.floor(val * 10) / 10;
+  },
+
+};
