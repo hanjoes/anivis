d430b7c570f884305a08d15ece477f17ead0cd43
Hanzhou Shi <hanzhou87@gmail.com>
2016-04-25 16:20:58 -0700

    Now filters can work with real data.

--- web/js/components/Filter.jsx
+++ web/js/components/Filter.jsx
@@ -2,36 +2,47 @@ var React = require("react");
 
 var Filter = React.createClass({
   handleChange() {
-    var selector = this._selector;
-    var selectedCategory = selector[selector.selectedIndex].value
-    this.props.inputHandler(this.props.attribute, selectedCategory);
+    var select = this._select;
+    var selectedCategory = select[select.selectedIndex].value
+    this.props.inputHandler(this.props.filter, selectedCategory);
+  },
+
+  getOneOption(filter, value, first = false) {
+    var message = filter["message"];
+    return (
+      <option value={value} key={filter["name"] + "_" + value}>
+      {first ? message : value}
+      </option>
+    )
   },
 
   render() {
+    var _c = this;
     var options = [];
-    var allKey = this.props.attribute + "_all";
-    options.push(
-      <option value={allKey} key={allKey}>
-          all
-      </option>
-    );
-    this.props.categories.forEach(function(categoryStat) {
-      for (var key in categoryStat) {
-        if (categoryStat.hasOwnProperty(key)) {
-          options.push(
-            <option value={key} key={key}>
-              {key}
-            </option>
-          );
-        }
-      }
+    var filter = this.props.filter;
+    var categories = this.props.categories;
+
+    // initialize the "none" option
+    options.push(_c.getOneOption(filter, "default", true));
+
+    // initialize any preset option
+    var preset = filter["preset"]
+    preset && preset.forEach(function(category) {
+      options.push(_c.getOneOption(filter, category));
     });
+
+    // initialize other options from data
+    // TODO:
+    categories && categories.forEach(function(category) {
+      options.push(_c.getOneOption(filter, category));
+    });
+
     return (
       <span>
-        {this.props.attribute}:
-        <select onChange={this.handleChange} ref={(c) => this._selector = c}>
-          {options}
-        </select>
+      {filter["name"]}:
+      <select onChange={this.handleChange} ref={(c) => this._select = c}>
+      {options}
+      </select>
       </span>
     );
   }
